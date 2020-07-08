1c8ffffd3b31372ab9141eda27ef72613f2a239e
Trey Lloyd <58454082+HUGHIII@users.noreply.github.com>
2020-07-08 15:22:03 -0500

    all finished except cal and classes, going back over it with solution from lecture

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,17 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-print('x is %d, y is %f, z is %s'%(x,y,z))
+
+# %s - String (or any object with a string representation, like numbers)
+
+# %d - Integers
+
+# %f - Floating point numbers
+
+# %.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+
+# %x/%X - Integers in hex representation (lowercase/uppercase)
+print('x is %d, y is %.2f, z is %s'%(x,y,z))
 
 # Use the 'format' string method to print the same thing
 sampleText ='x is {0}, y is {1}, z is {2}'
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+f = open("foo.txt","r")
+for lines in f:
+    print(lines)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f2 = open("bar.txt","w")
+
+for i in range(4):
+    f2.write("These are lines i made")
+
+f2.close()
+
+f3=open("bar.txt","r")
+for lines in f3:
+    print(lines)
